4beacc0f3c81a4afa3ab953b5c0beeba9245eb18
Rafa-Aragao <rafa.aragao05@gmail.com>
2025-10-15 10:43:34 -0300

    Added word count and character count

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-
+from stats import count_words
+from stats import num_characters
 def get_book_text(filepath):
     with open(filepath) as f: 
         file_contents =  f.read()
     return  file_contents
+
+
 def main():
     Frankenstein = get_book_text("books/frankenstein.txt") # type: ignore
-    print(Frankenstein)
-
+    Word_count = count_words(Frankenstein)
+    character_count = num_characters(Frankenstein)
     
+    print(f"Found {len(Word_count)} total words" )
+    print(character_count)
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,20 @@
+def count_words(book_string):
+    word_count = book_string.split()
+    return word_count
+def num_characters(book_string):
+    characters_dict = {}
+    characters_count = []
+    num = list(book_string)
+    for i in num:
+        if i.lower() == " ":
+            pass
+        else:
+            if i.lower() in characters_count:
+                characters_dict[i.lower()] += 1
+            else:
+                characters_count.append(i.lower())
+                characters_dict.update([(i.lower(), 1)])
+                
+    return characters_dict
+
+
